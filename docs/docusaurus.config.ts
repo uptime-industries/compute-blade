bd1829957fc4c4f40e295275bfff66bc547d2a4c
John William Bowen <johnwb1776@gmail.com>
2024-06-10 11:17:19 -0400

    removed blog

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           label: 'Website',
           href: 'https://computeblade.com',
@@ -89,15 +89,15 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Tutorial',
+        //       to: '/docs/intro',
+        //     },
+        //   ],
+        // },
         {
           title: 'Community',
           items: [
@@ -122,10 +122,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'Website',
               href: 'https://computeblade.com',
