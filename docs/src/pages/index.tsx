a1d858928048b77aaa62bc30024c94f4d2f67a82
John William Bowen <johnwb1776@gmail.com>
2024-08-21 17:54:39 -0400

    updated meta description

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -32,8 +32,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Documentation for Compute Blade bring the cloud home <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
