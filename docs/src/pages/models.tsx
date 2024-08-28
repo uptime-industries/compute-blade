b0454f07021fe46797532061b19fb0174396ac4d
John William Bowen <johnwb1776@gmail.com>
2024-08-28 10:36:07 -0400

    rename bladerunner

--- docs/src/pages/models.tsx
+++ docs/src/pages/models.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from '@docusaurus/router';
 import config from '../../docusaurus.config'
 function body() {
-  const path = `${config.baseUrl}blade/blade-runner`
+  const path = `${config.baseUrl}blade/bladerunner`
   return <Redirect to={path} />;
 }
 
