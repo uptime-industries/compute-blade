7801fc49afa09c73a0866dcea922da8d51218eb3
John William Bowen <johnwb1776@gmail.com>
2024-09-29 10:15:41 -0400

    bug fix

--- docs/src/pages/data-sheets.tsx
+++ docs/src/pages/data-sheets.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from '@docusaurus/router';
 import config from '../../docusaurus.config'
 function body() {
-  const path = `${config.baseUrl}/datasheets`
+  const path = `${config.baseUrl}datasheets`
   return <Redirect to={path} />;
 }
 
