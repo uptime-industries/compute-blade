c2946af9b1d5f1050ea993769b292a7941a22d5a
Vladimir Khomenok <34284073+khomenok@users.noreply.github.com>
2024-10-18 19:20:02 +0200

    Feat: Better Client Redirects (#35)
    * feat: added client redirects
    
    * feat: added client redirects

--- docs/src/pages/getting-started.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from '@docusaurus/router';
-import config from '../../docusaurus.config'
-function body() {
-  const path = `${config.baseUrl}blade/getting-started/assembly`
-  return <Redirect to={path} />;
-}
-
-export default body
