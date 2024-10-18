6f3df550a9351a77f8b744886b9dee022c9d6c3b
Vladimir Khomenok <khomenokvladimir@gmail.com>
2024-10-18 19:12:34 +0200

    feat: added client redirects

--- docs/src/pages/data-sheets.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from '@docusaurus/router';
-import config from '../../docusaurus.config'
-function body() {
-  const path = `${config.baseUrl}datasheets`
-  return <Redirect to={path} />;
-}
-
-export default body
