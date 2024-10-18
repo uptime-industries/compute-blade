ee3e0e4ceb4d72b5d5a7699319bd6a392f8ad5ff
John William Bowen <johnwb1776@gmail.com>
2024-10-18 08:20:28 -0400

    bug fix

--- docs/src/pages/getting-started.tsx
+++ docs/src/pages/getting-started.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from '@docusaurus/router';
 import config from '../../docusaurus.config'
 function body() {
-  const path = `${config.baseUrl}blade/getting-started/assembly/`
+  const path = `${config.baseUrl}blade/getting-started/assembly`
   return <Redirect to={path} />;
 }
 
--- docs/src/pages/models.tsx
+++ docs/src/pages/models.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from '@docusaurus/router';
 import config from '../../docusaurus.config'
 function body() {
-  const path = `${config.baseUrl}blade/bladerunner`
+  const path = `${config.baseUrl}bladerunner`
   return <Redirect to={path} />;
 }
 
