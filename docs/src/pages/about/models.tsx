9b5e34bf7401a1873f368922e261dae02da972d0
John William Bowen <johnwb1776@gmail.com>
2024-11-13 08:50:06 -0500

    added Blade and fan unit PCB models

--- docs/src/pages/about/models.tsx
+++ docs/src/pages/about/models.tsx
@@ -1,7 +1,7 @@
 import useIsBrowser from '@docusaurus/useIsBrowser';
 function body() {
   const isBrowser = useIsBrowser();
-  const path = `https://github.com/uptime-industries/compute-blade/tree/main/bladerunner`
+  const path = `https://github.com/uptime-industries/compute-blade/tree/main/models`
   const location = isBrowser ? window.location.href=path : path;
   return location;
 }
